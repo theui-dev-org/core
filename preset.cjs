de49d2f754b862377fdd5d4fe1ae5bd5e31a643e
M. B. Parvez Ron <mbparvezme@gmail.com>
2022-06-30 23:24:49 +0600

    v0.18.0

--- preset.cjs
+++ preset.cjs
@@ -1,7 +1,60 @@
+// const defaultTheme = require('tailwindcss/defaultTheme')
+
+// let withOpacity = (variableName) => {
+//   return ({ opacityValue }) => {
+//     if (opacityValue !== undefined) {
+//       return `rgba(var(${variableName}), ${opacityValue})`
+//     }
+//     return `rgb(var(${variableName}))`
+//   }
+// }
+
+// let brandColors = {
+//   "brand"		        : withOpacity("--brand"),
+//   "brand-active"    : withOpacity("--brand-active"),
+// }
+
+// let textColors  = {
+// 	"on-brand"    		: withOpacity("--text-on-brand"),
+// 	"primary"    		  : withOpacity("--ui-text-primary"),
+// }
+
+// let bgColors    = {
+//   "primary"					: withOpacity("--ui-bg-primary"),
+//   "secondary"       : withOpacity("--ui-bg-secondary"),
+//   "tertiary"        : withOpacity("--ui-bg-tertiary"),
+// }
+
 const config = {
   mode: "jit",
   darkMode: 'class',
-  theme: {},
+  theme: {
+    extend: {
+      colors: {
+        brand           : 'rgb(var(--ui-brand) / <alpha-value>)',
+        "brand-active"  : 'rgb(var(--ui-brand-active) / <alpha-value>)',
+        "on-brand"      : 'rgb(var(--ui-text-on-brand) / <alpha-value>)',
+        primary         : 'rgb(var(--ui-bg-primary) / <alpha-value>)',
+        secondary       : 'rgb(var(--ui-bg-secondary) / <alpha-value>)',
+        tertiary        : 'rgb(var(--ui-bg-tertiary) / <alpha-value>)',
+      },
+      textColor : {
+        brand           : 'rgb(var(--ui-brand) / <alpha-value>)',
+        "brand-active"  : 'rgb(var(--ui-brand-active) / <alpha-value>)',
+        "on-brand"      : 'rgb(var(--ui-text-on-brand) / <alpha-value>)',
+        default         : 'rgb(var(--ui-text-default) / <alpha-value>)',
+      },
+      backgroundColor : {
+        brand           : 'rgb(var(--ui-brand) / <alpha-value>)',
+        "brand-active"  : 'rgb(var(--ui-brand-active) / <alpha-value>)',
+        primary         : 'rgb(var(--ui-bg-primary) / <alpha-value>)',
+        secondary       : 'rgb(var(--ui-bg-secondary) / <alpha-value>)',
+        tertiary        : 'rgb(var(--ui-bg-tertiary) / <alpha-value>)',
+      },
+			fontWeight: ['dark'],
+			fill : ['dark'],
+		},
+  },
   plugins: [
     require('@tailwindcss/forms'),
   ],
--- types.ts
+++ types.ts
@@ -1,220 +1,211 @@
 type ANIMATE_SPEED_TYPE  = 'slower' | 'slow' | 'normal' | 'fast' | 'faster'
 type ButtonType          = 'button'| 'reset'| 'submit'
 type INPUT_TYPE          = 'checkbox'| 'color'| 'date'| 'datetime-local'| 'email'| 'month'| 'number'| 'password'| 'radio'| 'range'| 'search'| 'tel'| 'text'| 'textarea' | 'time'| 'url' | 'week'
-type ROUNDED_TYPE        = 'sm' | 'md' | 'lg' | 'xl' | 'full' | 'none' | boolean
-type SHADOW_TYPE         = 'sm' | 'md' | 'lg' | 'xl' | '2xl' | 'inner' | 'none' | boolean
+type ROUNDED_TYPE        = 'sm' | 'md' | 'lg' | 'xl' | 'full' | 'none'
+type SHADOW_TYPE         = 'sm' | 'md' | 'lg' | 'xl' | '2xl' | 'inner' | 'none'
+
+type SITE = {
+  siteName          : string,
+  slogan           ?: string,
+  description       : string,
+  keywords         ?: string,
+  logoPath         ?: string,
+  imgPath          ?: string,
+  siteSchema       ?: string,
+  businessSchema   ?: string,
+  lang             ?: string,
+  fbAppID          ?: string,
+  pageType         ?: string,
+  twitterCardType  ?: string,
+  twitterID        ?: string,
+  twitterCreatorID ?: string,
+  video            ?: string,
+  gtmCode          ?: string,
+}
 
 type CORE = {
   isRounded             : boolean,
   toggleTheme           : boolean|'system',
   animate               : boolean,
   animateSpeed          : ANIMATE_SPEED_TYPE,
-  notificationPosition  : 'top-right' | 'top-center' | 'top-left' | 'bottom-right' | 'bottom-center' | 'bottom-left',
   reset                 : boolean,
 }
 
 type ACCORDION_CONFIG_TYPE = {
-  animate           : boolean,
-  animateSpeed      : ANIMATE_SPEED_TYPE,
-  bodyActiveCls     : string,
-  bodyCls           : string,
-  cls               : string,
-  contentActiveCls  : string,
-  contentCls        : string,
-  hover             : string,
-  reset             : boolean,
-  rounded           : ROUNDED_TYPE,
-  size              : 'compact' | 'default' | 'large',
-  type              : 'default' | 'flat',
-  titleActiveCls    : Object|string,
-  titleCls          : string,
+  animateSpeed          ?: ANIMATE_SPEED_TYPE,
+  containerActiveClass  ?: Object|string,
+  containerClass        ?: Object|string,
+  contentActiveClass    ?: Object|string,
+  contentClass          ?: Object|string,
+  rounded               ?: ROUNDED_TYPE,
+  size                  ?: 'compact' | 'default' | 'large',
+  titleActiveClass      ?: Object|string,
+  titleClass            ?: Object|string,
+  type                  ?: 'default' | 'flush',
 }
 
 type BREADCRUMB_DATA_TYPE = {
   active ?: boolean,
-  text: string,
-  href: string,
+  text    : string,
+  href    : string,
 }
 
 type BUTTON_CONFIG_TYPE = {
-  active        : boolean|string,
-  animate       : boolean,
-  animateSpeed  : ANIMATE_SPEED_TYPE,
-  cls           : string,
-  hover         : boolean|string,
-  outline       : boolean,
-  reset         : boolean,
-  rounded       : ROUNDED_TYPE,
-  shadow        : SHADOW_TYPE,
-  size          : 'xs' | 'sm' | 'md' | 'lg' | 'xl' | 0,
-  theme         : string|undefined,
+  activeClass   ?: boolean|string,
+  class         ?: boolean|string,
+  outline       ?: boolean,
+  rounded       ?: ROUNDED_TYPE,
+  shadow        ?: SHADOW_TYPE,
+  size          ?: 'xs' | 'sm' | 'md' | 'lg' | 'xl' | 0,
 }
 
 type COLLAPSE_CONFIG_TYPE = {
-  animate       : boolean,
-  animateSpeed  : ANIMATE_SPEED_TYPE,
-}
-
-type DD_CONFIG_TYPE = {
-  animate             : boolean,
-  animateSpeed        : ANIMATE_SPEED_TYPE,
-  animation           : 'slide-left' | 'slide-up' | 'slide-right' | 'slide-down' | 'fade' | 'zoom-in' | 'zoom-out',
-  alignRight          : boolean,
-  backdrop            : boolean,
-  block               : boolean,
-  closeOnClick        : boolean,
-  closeOnEsc          : boolean,
-  closeOnOutsideClick : boolean,
-  cls                 : string,
-  itemConfig          : DD_ITEM_CONFIG_TYPE,
-  reset               : boolean,
-  rounded             : ROUNDED_TYPE,
-  size                : 'sm' | 'md' | 'lg' | 'full' | 'auto',
-  standalone          : boolean,
-  theme               : string
+  animateSpeed  ?: ANIMATE_SPEED_TYPE,
 }
 
 type DD_ITEM_CONFIG_TYPE = {
-  active          : boolean,
-  activeTheme     : string,
-  dividerTheme    : string,
-  headerTheme     : string,
-  iconAfterSize   : number,
-  iconAfterTheme  : string,
-  iconBeforeSize  : number,
-  iconBeforeTheme : string,
-  reset           : boolean,
-  theme           : string,
+  activeClass     ?: string,
+  class           ?: string,
+  dividerClass    ?: string,
+  headerClass     ?: string
 }
+
+type DD_CONFIG_TYPE = {
+  animateSpeed        ?: ANIMATE_SPEED_TYPE,
+  animation           ?: 'slide-left' | 'slide-up' | 'slide-right' | 'slide-down' | 'fade' | 'zoom-in' | 'zoom-out',
+  alignRight          ?: boolean,
+  backdrop            ?: boolean,
+  closeOnClick        ?: boolean,
+  closeOnEsc          ?: boolean,
+  closeOnOutsideClick ?: boolean,
+  itemConfig          ?: DD_ITEM_CONFIG_TYPE,
+  rounded             ?: ROUNDED_TYPE,
+  size                ?: 'sm' | 'md' | 'lg' | 'full' | 'auto',
+  standalone          ?: boolean,
+}
+
 type DD_DATA = {
-  active ?: boolean,
-  divider ?: boolean,
-  header ?: boolean,
-  name: string,
-  href: string,
-  iconBefore ?: string,
-  iconAfter ?: string,
-  text ?: string,
+  active     ?: boolean,
+  url        ?: string,
+  text       ?: string,
+  type        : 'link' | 'divider' | 'header',
 }
 
 type MODAL_CONFIG_TYPE = {
-  animate         : boolean,
-  animateSpeed    : ANIMATE_SPEED_TYPE,
-  animation       : 'slide-down' | 'slide-up' | 'fade' | 'zoom-in' | 'zoom-out',
-  backdrop        : boolean,
-  closeBtn        : boolean,
-  closeOnEsc      : boolean,
-  bodyCls         : string,
-  position        : 'top' | 'center' | 'bottom',
-  reset           : boolean,
-  rounded         : ROUNDED_TYPE,
-  size            : 'sm' | 'md' | 'lg' | 'full',
-  staticBackdrop  : boolean,
-  theme           : string,
+  animate         ?: boolean,
+  animateSpeed    ?: ANIMATE_SPEED_TYPE,
+  animation       ?: 'slide-down' | 'slide-up' | 'fade' | 'zoom-in' | 'zoom-out',
+  backdrop        ?: boolean,
+  bodyClass     ?: string,
+  closeBtn        ?: boolean,
+  closeOnEsc      ?: boolean,
+  contentClass  ?: string,
+  position        ?: 'top' | 'center' | 'bottom',
+  reset           ?: boolean,
+  rounded         ?: ROUNDED_TYPE,
+  size            ?: 'sm' | 'md' | 'lg' | 'full',
+  staticBackdrop  ?: boolean,
 }
 
 type NAV_CONFIG_TYPE = {
-  animate               : boolean,
-  animateSpeed          : ANIMATE_SPEED_TYPE,
-  cls                   : string,
-  height                : 'sm' | 'md' | 'lg' | 'xl',
-  reset                 : boolean,
-  rounded               : ROUNDED_TYPE,
-  scrollAmountToShrink  : number,
-  scrollAmountToHide    : number,
-  scrollBehavior        : 'fixed' | 'default' | 'shrinkOnScrollDown' | 'hideOnScrollDown' | 'shrinkAndHide'
-  theme                 : string,
+  animate               ?: boolean,
+  animateSpeed          ?: ANIMATE_SPEED_TYPE,
+  class               ?: string,
+  height                ?: 'sm' | 'md' | 'lg' | 'xl',
+  reset                 ?: boolean,
+  rounded               ?: ROUNDED_TYPE,
+  scrollAmountToShrink  ?: number,
+  scrollAmountToHide    ?: number,
+  scrollBehavior        ?: 'fixed' | 'default' | 'shrinkOnScrollDown' | 'hideOnScrollDown' | 'shrinkAndHide'
+  scrollClass         ?: string,
 }
 
 type NOTIFY_CONFIG_TYPE = {
-  id          : string,
-  position    : 'top-right' | 'top-center' | 'top-left' | 'bottom-right' | 'bottom-center' | 'bottom-left',
-  removeAfter : number,
-  reset       : boolean,
-  rounded     : ROUNDED_TYPE,
-  style       : string,
-  theme       : string|undefined,
+  class       ?: string,
+  removeAfter   ?: number,
+  removeOnClick ?: boolean,
+  reset         ?: boolean,
+  rounded       ?: ROUNDED_TYPE,
+  style         ?: 'default'|'flat',
 }
 
 type OFFCANVAS_CONFIG_TYPE = {
-  animate         : boolean,
-  animateSpeed    : ANIMATE_SPEED_TYPE,
-  backdrop        : boolean,
-  closeOnEsc      : boolean,
-  cls             : string,
-  position        : 'top' | 'right' | 'bottom' | 'left',
-  staticBackdrop  : boolean,
-  theme           : string,
+  animate         ?: boolean,
+  animateSpeed    ?: ANIMATE_SPEED_TYPE,
+  backdrop        ?: boolean,
+  closeOnEsc      ?: boolean,
+  position        ?: 'top' | 'right' | 'bottom' | 'left',
+  staticBackdrop  ?: boolean,
 }
 
 type PROGRESS_CONFIG_TYPE = {
-  animate         : boolean,
-  animateSpeed    : ANIMATE_SPEED_TYPE,
-  height          : 'px' | 'sm' | 'md' | 'lg' | 'xl' | 'none',
-  reset           : boolean,
-  rounded         : ROUNDED_TYPE,
-  roundedProgress : ROUNDED_TYPE,
-  textTheme       : string,
-  trackTheme      : string,
-  theme           : string,
+  barClass      ?: string,
+  height          ?: 'px' | 'sm' | 'md' | 'lg' | 'xl' | 'none',
+  rounded         ?: ROUNDED_TYPE,
+  roundedProgress ?: ROUNDED_TYPE,
+}
+
+type TABLE_CONFIG_TYPE = {
+  borderColor       ?: string,
+  border            ?: 'x'|'y'|'both'|'none',
+  hover             ?: boolean|string,
+  reset             ?: boolean,
+  size              ?: 'sm'|'md'|'lg',
+  stripe            ?: boolean|string,
+  containerClass  ?: string,
+  tableClass      ?: string,
+  theadClass      ?: string,
+  tbodyClass      ?: string,
+  trClass         ?: string,
 }
 
 type TAB_CONFIG_TYPE = {
-  activeTabTheme  : object|string,
-  animate         : boolean,
-  animateSpeed    : ANIMATE_SPEED_TYPE,
-  panelTheme      : string,
-  reset           : boolean,
-  rounded         : ROUNDED_TYPE,
-  shadow          : SHADOW_TYPE,
-  tabCls          : object|string,
-  tabTheme        : object|string,
+  activeTabClass  ?: object|string,
+  reset             ?: boolean,
+  rounded           ?: ROUNDED_TYPE,
+  shadow            ?: SHADOW_TYPE,
+  tabClass        ?: object|string,
 }
 
 type TOOLTIP_CONFIG_TYPE = {
-  animate       : boolean,
-  animateSpeed  : ANIMATE_SPEED_TYPE,
-  animation     : 'fade' | 'slide' | 'zoom-in' | 'zoom-out',
-  reset         : boolean,
-  rounded       : ROUNDED_TYPE,
-  // theme         : string,
+  animate       ?: boolean,
+  animateSpeed  ?: ANIMATE_SPEED_TYPE,
+  animation     ?: 'fade' | 'slide' | 'zoom-in' | 'zoom-out',
+  bgColor       ?: string,
+  inline        ?: boolean,
+  position      ?: 'left' | 'top' | 'right' | 'bottom',
+  reset         ?: boolean,
+  rounded       ?: ROUNDED_TYPE
 }
-
 
 // FORM CONTROLS AND DATA TYPES
 type INPUT_CONFIG_TYPE = {
-  animate       : boolean,
-  animateSpeed  : ANIMATE_SPEED_TYPE,
-  appearance    : 'default' | 'flat' | 'material',
-  containerCls  : string,
-  fileBtnCls   ?: string,
-  fileBtnTheme ?: string,
-  grow          : boolean,
-  inputCls      : string|object,
-  inputFocusCls : string|object,
-  labelStyle    : LABEL_STYLE_TYPE,
-  marginBottom  : 0 | 4 | 8 | 12,
-  reset         : boolean,
-  rounded       : ROUNDED_TYPE,
-  size          : 'none' |'xs' | 'sm' | 'md' | 'lg' | 'xl',
-  theme         : string,
+  appearance        ?: 'default' | 'flat' | 'material',
+  containerClass  ?: string,
+  fileBtnClass    ?: string,
+  grow              ?: boolean,
+  inputClass      ?: string|object,
+  inputFocusClass ?: string|object,
+  labelStyle        ?: LABEL_STYLE_TYPE,
+  marginBottom      ?: 0 | 4 | 8 | 12,
+  reset             ?: boolean,
+  rounded           ?: ROUNDED_TYPE,
+  size              ?: 'none' |'xs' | 'sm' | 'md' | 'lg' | 'xl',
 }
 
 type LABEL_STYLE_TYPE = {
-  cls           : string,
-  color         : string,
-  fontWeight    : string,
-  marginBottom  : 0 | 2 | 4 | 6,
-  textSize      : string,
-  transform     : string,
+  class       ?: string,
+  color         ?: string,
+  marginBottom  ?: 0 | 2 | 4 | 6,
+  textSize      ?: string,
 }
 
 type CHECKBOX_RADIO_CONFIG_TYPE = {
-  cls       : string,
-  disabled  : boolean,
-  rounded   : ROUNDED_TYPE,
-  size      : 'md' | 'lg' | 'xl',
-  theme     : string,
+  class     ?: string,
+  disabled    ?: boolean,
+  rounded     ?: ROUNDED_TYPE,
+  reset       ?: boolean,
+  size        ?: 'md' | 'lg' | 'xl'
 }
 
 type SELECT_DATA_TYPE = {
@@ -224,9 +215,9 @@ type SELECT_DATA_TYPE = {
   value    ?: any,
 }
 
-export type { ANIMATE_SPEED_TYPE, ButtonType, INPUT_TYPE, ROUNDED_TYPE, SHADOW_TYPE, CORE,
+export type { SITE, CORE, ANIMATE_SPEED_TYPE, ButtonType, INPUT_TYPE, ROUNDED_TYPE, SHADOW_TYPE,
   ACCORDION_CONFIG_TYPE, BREADCRUMB_DATA_TYPE, BUTTON_CONFIG_TYPE, COLLAPSE_CONFIG_TYPE,
-  DD_CONFIG_TYPE, DD_ITEM_CONFIG_TYPE, DD_DATA, MODAL_CONFIG_TYPE, NAV_CONFIG_TYPE,
+  DD_CONFIG_TYPE, DD_DATA, MODAL_CONFIG_TYPE, NAV_CONFIG_TYPE,
   NOTIFY_CONFIG_TYPE, OFFCANVAS_CONFIG_TYPE, PROGRESS_CONFIG_TYPE, TAB_CONFIG_TYPE,
-  TOOLTIP_CONFIG_TYPE, INPUT_CONFIG_TYPE, LABEL_STYLE_TYPE, CHECKBOX_RADIO_CONFIG_TYPE,
-  SELECT_DATA_TYPE}
+  TABLE_CONFIG_TYPE, TOOLTIP_CONFIG_TYPE, INPUT_CONFIG_TYPE, LABEL_STYLE_TYPE,
+  CHECKBOX_RADIO_CONFIG_TYPE, SELECT_DATA_TYPE}
